37dd9e4519d0d6918442dcbbbb8bee9807144805
samuel_ndjouli <samuelndj.997@gmail.com>
2024-03-06 14:19:28 +0100

    imported generate_quizz function

--- quizz/app.py
+++ quizz/app.py
@@ -1,16 +1,32 @@
-from flask import Flask, render_template
-
-# Importez la fonction chat_completion depuis le fichier openai.py
-# from services.openai import chat_completion
+from flask import Flask, render_template, jsonify
+from services.generate_quizz import generate_quiz
+from services.openai import chat_completion
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+
+load_dotenv()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# @app.route("/test_openai")
-# def test_openai():
-    # Appelez la fonction chat_completion et affichez le résultat dans la réponse de la requête
-    # return str(chat_completion)
+@app.route("/test_openai")
+def test_openai():
+    return str(chat_completion)
 
+@app.route("/generate_quiz", methods=['GET'])
+def handle_generate_quiz():
+    try:
+        subject = "capitales"
+        quiz = generate_quiz(subject)
+        if quiz:
+            return jsonify({"quiz": quiz})
+        else:
+            return jsonify({"error": "Failed to generate quiz."})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
